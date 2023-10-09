e2aeef70e1cff95d20faaef2a597d738a970ddb9
Svetlana <svetlana.rubashkina2401@yandex.ru>
2023-10-09 20:56:38 +0300

    Third commit with screenshot

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://e0317aa3-9438-4f95-92d3-c94a3240de30.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://e7ac6390-525e-4daa-9b0a-3862ff9af0bb.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/v1/orders/track?t="
